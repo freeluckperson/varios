cbaa37199c3fb3a2a84e33d608ff5d88c8008042
freeluckperson <gestionecesaria@gmail.com>
2024-11-15 10:40:23 -0400

    15-nov-2024

---
+++ ejercicios-java/TablaDeMultiplicar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TablaDeMultiplicar {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Intruduce un tabla entero");
+        int tabla = scanner.nextInt();
+        int producto = 0;
+
+        int i = 1;
+        while (i <= 10) {
+            producto = i * tabla;
+            System.out.println(tabla + "x" + i + " = " + producto);
+            i++;
+        }
+
+        scanner.close();
+    }
+}
