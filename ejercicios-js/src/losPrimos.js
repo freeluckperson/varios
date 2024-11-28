1a854b8d28e386edc50d9147fb16b835c7281ebd
freeluckperson <gestionecesaria@gmail.com>
2024-11-27 20:18:11 -0400

    27-nov-2024

--- ejercicios-js/src/losPrimos.js
+++ ejercicios-js/src/losPrimos.js
@@ -10,10 +10,6 @@ function soloPrimos(num) {
 }
 const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let arrayDePrimos = [];
-/* let arrayDePrimos = array.filter((ele) => soloPrimos(ele));
-console.log(arrayDePrimos); */
-//2-3-5-7
-/* console.log(array2); */
 
 for (const num of array) {
   if (soloPrimos(num)) {
