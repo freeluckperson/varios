6ef38d10e7c22420763dbed3745ac3f9044e8e2d
freeluckperson <gestionecesaria@gmail.com>
2024-12-01 17:48:19 -0400

    1-dic-2024

--- ejercicios-java/Herencia.java
+++ ejercicios-java/Herencia.java
@@ -1,38 +1,35 @@
-class Fruit1 {
-    String fruit1;
 
-    Fruit1(String fruit1) {
-        this.fruit1 = fruit1;
+class Persona {
+    String name;
+
+    Persona(String name) {
+        this.name = name;
     }
 
-    public void descriptionfruit1() {
-        System.out.println("This is a " + fruit1);
+    public void presentarme() {
+        System.out.println("Mi nombre es " + name);
     }
 }
 
-class Fruit2 extends Fruit1 {
-    String fruit2;
+class Programador extends Persona {
+    String rol;
 
-    Fruit2(String fruit2, String fruit1) {
-        super(fruit1);
-        this.fruit2 = fruit2;
-    }
-
-    public void descriptionfruit2() {
-        System.out.println("This is a " + fruit2);
+    Programador(String rol, String name) {
+        super(name);
+        this.rol = rol;
     }
 
     @Override
-    public void descriptionfruit1() {
-        System.out.println("This is a " + fruit2 + " and " + fruit1);
+    public void presentarme() {
+        System.out.println("Mi nombre es " + name + " y soy programador " + rol);
     }
 
 }
 
 class Herencia {
     public static void main(String[] args) {
-        Fruit2 cocktail = new Fruit2("piña", "coco");
-        cocktail.descriptionfruit1();
-        cocktail.descriptionfruit2();
+        Programador programador1 = new Programador("Full stack", "Erick");
+        programador1.presentarme();
+
     }
 }
