e8abbd2035c756b98a0227e8e220293adccb6ecb
freeluckperson <gestionecesaria@gmail.com>
2025-01-02 20:05:49 -0400

    02-ene-2025

--- ejercicios-js/src/newsCrypt.js
+++ ejercicios-js/src/newsCrypt.js
@@ -24,6 +24,7 @@ if (longCondition)
     
     // Establecer niveles de TP y SL
     strategy.exit("Take Profit/Stop Loss", "Long", limit=takeProfit, stop=stopLoss)
+
 //======================================================================================
 
 //@version=6
@@ -60,3 +61,129 @@ plot(smoothingMA, "EMA-based MA", color=color.yellow, display = enableMA ? displ
 bbUpperBand = plot(smoothingMA + smoothingStDev, title = "Upper Bollinger Band", color=color.green, display = isBB ? display.all : display.none)
 bbLowerBand = plot(smoothingMA - smoothingStDev, title = "Lower Bollinger Band", color=color.green, display = isBB ? display.all : display.none)
 fill(bbUpperBand, bbLowerBand, color= isBB ? color.new(color.green, 90) : na, title="Bollinger Bands Background Fill", display = isBB ? display.all : display.none)
+
+//======================================================================================
+//CODIGO PARA OPERAR EN LONG
+
+//@version=5
+indicator("Estrategia Mejorada con Señales Alcistas", overlay=true)
+
+// Parámetros
+rsiLength = input.int(14, title="Longitud RSI")
+rsi = ta.rsi(close, rsiLength)
+smaLength = input.int(50, title="Longitud SMA")
+sma = ta.sma(close, smaLength)
+
+// Parámetros para las EMAs
+ema1Length = input.int(50, title="Longitud EMA 1")
+ema2Length = input.int(100, title="Longitud EMA 2")
+ema3Length = input.int(150, title="Longitud EMA 3")
+
+// Cálculo de las EMAs
+ema1 = ta.ema(close, ema1Length)
+ema2 = ta.ema(close, ema2Length)
+ema3 = ta.ema(close, ema3Length)
+
+// Parámetros para Take Profit y Stop Loss
+takeProfitPercent = input.float(0.7, title="Take-Profit (%)", minval=0.0)
+stopLossPercent = input.float(2, title="Stop-Loss (%)", minval=0.0)
+
+// Variables para seguimiento
+var float entryPrice = na
+var bool inPosition = false
+var float takeProfitPrice = na
+var float stopLossPrice = na
+
+// Condiciones para entrada
+longCondition = close > ema1 and close > ema2 and close > ema3 and ema1 > ema2 and ema2 > ema3 and rsi > 55 and ta.crossover(rsi, 55)
+
+if (longCondition and not inPosition)
+    entryPrice := close
+    takeProfitPrice := entryPrice * (1 + takeProfitPercent / 100)
+    stopLossPrice := entryPrice * (1 - stopLossPercent / 100)
+    inPosition := true
+    label.new(bar_index, low, "Compra - Precio: " + str.tostring(entryPrice, "#.##"), color=color.green, textcolor=color.white)
+
+// Lógica para salida
+if (inPosition)
+    // Si se alcanza el Take Profit
+    if (close >= takeProfitPrice)  
+        label.new(bar_index, high, "Venta - Take Profit: " + str.tostring(takeProfitPrice, "#.##"), color=color.green, textcolor=color.white)
+        inPosition := false
+
+    // Si se alcanza el Stop Loss
+    if (close <= stopLossPrice)  
+        label.new(bar_index, high, "Venta - Stop Loss: " + str.tostring(stopLossPrice, "#.##"), color=color.red, textcolor=color.white)
+        inPosition := false
+
+// Visualización de niveles de TP y SL
+plot(inPosition ? takeProfitPrice : na, color=color.green, style=plot.style_stepline, title="Take Profit")
+plot(inPosition ? stopLossPrice : na, color=color.red, style=plot.style_stepline, title="Stop Loss")
+
+// Dibujar las EMAs en el gráfico con los colores deseados
+plot(ema1, color=color.yellow, title="EMA 50", linewidth=2)
+plot(ema2, color=color.blue, title="EMA 100", linewidth=2)
+plot(ema3, color=color.red, title="EMA 150", linewidth=2)
+
+//======================================================================================
+// CODIGO PARA OPERAR EN SHORT
+
+//@version=5
+indicator("Estrategia Mejorada con Señales Bajistas", overlay=true)
+
+// Parámetros
+rsiLength = input.int(14, title="Longitud RSI")
+rsi = ta.rsi(close, rsiLength)
+smaLength = input.int(50, title="Longitud SMA")
+sma = ta.sma(close, smaLength)
+
+// Parámetros para las EMAs
+ema1Length = input.int(50, title="Longitud EMA 1")
+ema2Length = input.int(100, title="Longitud EMA 2")
+ema3Length = input.int(150, title="Longitud EMA 3")
+
+// Cálculo de las EMAs
+ema1 = ta.ema(close, ema1Length)
+ema2 = ta.ema(close, ema2Length)
+ema3 = ta.ema(close, ema3Length)
+
+// Parámetros para Take Profit y Stop Loss
+takeProfitPercent = input.float(0.7, title="Take-Profit (%)", minval=0.0)
+stopLossPercent = input.float(2, title="Stop-Loss (%)", minval=0.0)
+
+// Variables para seguimiento
+var float entryPrice = na
+var bool inPosition = false
+var float takeProfitPrice = na
+var float stopLossPrice = na
+
+// Condiciones para entrada en corto
+shortCondition = close < ema1 and close < ema2 and close < ema3 and ema1 < ema2 and ema2 < ema3 and rsi < 45 and ta.crossunder(rsi, 45)
+
+if (shortCondition and not inPosition)
+    entryPrice := close
+    takeProfitPrice := entryPrice * (1 - takeProfitPercent / 100) // TP para corto
+    stopLossPrice := entryPrice * (1 + stopLossPercent / 100) // SL para corto
+    inPosition := true
+    label.new(bar_index, high, "Venta - Precio: " + str.tostring(entryPrice, "#.##"), color=color.red, textcolor=color.white)
+
+// Lógica para salida en corto
+if (inPosition)
+    // Si se alcanza el Take Profit
+    if (close <= takeProfitPrice)  
+        label.new(bar_index, low, "Cierre - Take Profit: " + str.tostring(takeProfitPrice, "#.##"), color=color.green, textcolor=color.white)
+        inPosition := false
+
+    // Si se alcanza el Stop Loss
+    if (close >= stopLossPrice)  
+        label.new(bar_index, low, "Cierre - Stop Loss: " + str.tostring(stopLossPrice, "#.##"), color=color.red, textcolor=color.white)
+        inPosition := false
+
+// Visualización de niveles de TP y SL
+plot(inPosition ? takeProfitPrice : na, color=color.green, style=plot.style_stepline, title="Take Profit")
+plot(inPosition ? stopLossPrice : na, color=color.red, style=plot.style_stepline, title="Stop Loss")
+
+// Dibujar las EMAs en el gráfico con los colores deseados
+plot(ema1, color=color.yellow, title="EMA 50", linewidth=2)
+plot(ema2, color=color.blue, title="EMA 100", linewidth=2)
+plot(ema3, color=color.red, title="EMA 150", linewidth=2)
--- ejercicios-js/src/scryptsGanadores.js
+++ ejercicios-js/src/scryptsGanadores.js
@@ -1,21 +1,136 @@
+/////===============================================================================================
+
+// "NO BORRAR" Codigo que genera muchas señales al dia facil de operar "NO BORRAR"
+
+// This source code is subject to the terms of the Mozilla Public License 2.0 at https://mozilla.org/MPL/2.0/
+
+//@version=5
+indicator('Swing Points & FVG', overlay=true, max_labels_count=500)
+
+var inputHighlightIntermediates = input(true, 'Highlight Intermediate Swing Highs and Lows')
+var inputIntermediateSize = input.string('Tiny', 'Intermediate Swing Label Size', options=['Auto', 'Tiny', 'Small', 'Normal'])
+var inputShowShortTermSwings = input(false, 'Short Term Swings')
+var inputPivotBars = input(2, 'Pivot Candles')
+var highcolor = input.color(color.red, title="High", tooltip="select a color for ST/IT Highs")
+var lowcolor = input.color(color.green, title="Low", tooltip="select a color for ST/IT Lows")
+
+var intermediateLabelSize = inputIntermediateSize == 'Auto' ? size.auto : inputIntermediateSize == 'Tiny' ? size.tiny : inputIntermediateSize == 'Small' ? size.small : size.normal
+
+// Parallel arrays for Swing High Data
+var float[] swingHighs = array.new_float()
+var label[] swingHighLabels = array.new_label()
+var bool[] swingHighIsHigher = array.new_bool()
+var bool[] swingHighIsIntermediate = array.new_bool()
+
+// Parallel arrays for Swing Low Data
+var float[] swingLows = array.new_float()
+var label[] swingLowLabels = array.new_label()
+var bool[] swingLowIsLower = array.new_bool()
+var bool[] swingLowIsIntermediate = array.new_bool()
+
+// see last element pushed an an array
+array_peek(arrayId) =>
+    int arraySize = array.size(arrayId)
+    if arraySize > 0
+        array.get(arrayId, arraySize - 1)
+    else
+        na
+
+// filters out swings that were not intermediate swings after all swings have been recorded.
+remove_short_term_swings() =>
+    if not inputShowShortTermSwings and barstate.islast
+        var swingHighCount = array.size(swingHighLabels)
+        var swingLowCount = array.size(swingLowLabels)
+
+        for i = 0 to swingHighCount - 1 by 1
+            if not array.get(swingHighIsIntermediate, i)
+                label.delete(array.get(swingHighLabels, i))
+
+        for i = 0 to swingLowCount - 1 by 1
+            if not array.get(swingLowIsIntermediate, i)
+                label.delete(array.get(swingLowLabels, i))
+
+
+// detect swing highs and lows
+swingHighPrice = ta.pivothigh(high, inputPivotBars, inputPivotBars)
+swingLowPrice = ta.pivotlow(low, inputPivotBars, inputPivotBars)
+
+
+if not na(swingHighPrice)
+    lastSwingHighPrice = array_peek(swingHighs)
+    isHigherHigh = swingHighPrice > lastSwingHighPrice
+    isLowerHigh = swingHighPrice < lastSwingHighPrice
+    swingLabel = label.new(bar_index[inputPivotBars], swingHighPrice, color=highcolor, yloc=yloc.price, style=label.style_circle, size=size.tiny)
+
+    // check if last swing high was intermediate
+    if isLowerHigh and array_peek(swingHighIsHigher)
+        // lower high after a higher high means the previous swing high was intermediate
+        if inputHighlightIntermediates
+            label.set_size(array_peek(swingHighLabels), intermediateLabelSize)
+            array.set(swingHighIsIntermediate, array.size(swingHighIsIntermediate) - 1, true)
+
+    // save swing
+    array.push(swingHighs, swingHighPrice)
+    array.push(swingHighLabels, swingLabel)
+    array.push(swingHighIsHigher, isHigherHigh)
+    array.push(swingHighIsIntermediate, false)  // can be filled in later, if a lower high forms next
+
+if not na(swingLowPrice)
+    lastSwingLowPrice = array_peek(swingLows)
+    isHigherLow = swingLowPrice > lastSwingLowPrice
+    isLowerLow = swingLowPrice < lastSwingLowPrice
+    swingLabel = label.new(bar_index[inputPivotBars], swingLowPrice, color=lowcolor, yloc=yloc.price, style=label.style_circle, size=size.tiny)
+
+    // check if last swing low was intermediate
+    if isHigherLow and array_peek(swingLowIsLower)
+        // higher low after a lower low means the previous swing low was intermediate
+        if inputHighlightIntermediates
+            label.set_size(array_peek(swingLowLabels), intermediateLabelSize)
+            array.set(swingLowIsIntermediate, array.size(swingLowIsIntermediate) - 1, true)
+
+    // save swing
+    array.push(swingLows, swingLowPrice)
+    array.push(swingLowLabels, swingLabel)
+    array.push(swingLowIsLower, isLowerLow)
+    array.push(swingLowIsIntermediate, false)  // can be filled in later if a higher low forms next
+
+
+remove_short_term_swings()
+
+
+//IMBALANCE
+show_IMB = input.bool(true, "Imbalance", group='IMBALANCE')
+imbalancecolor = input.color(color.yellow, title="Imbalance Color", group='IMBALANCE')
+fvgTransparency  = input(title="Transparency", defval=60, group='IMBALANCE')
+fvgboxLength = input.int(title='Length', defval=0, group='IMBALANCE')
+
+fvgisUp(index) =>
+    close[index] > open[index]
+
+fvgisDown(index) =>
+    close[index] < open[index]
+
+
+fvgisObUp(index) =>
+    fvgisDown(index + 1) and fvgisUp(index) and close[index] > high[index + 1]
+
+
+fvgisObDown(index) =>
+    fvgisUp(index + 1) and fvgisDown(index) and close[index] < low[index + 1]
+
+
+
+
+bullishFvg = low[0] > high[2]
+bearishFvg = high[0] < low[2]
+if bullishFvg and show_IMB
+    box.new(left=bar_index - 1, top=low[0], right=bar_index + fvgboxLength, bottom=high[2], bgcolor=color.new(imbalancecolor, fvgTransparency), border_color=color.new(imbalancecolor, fvgTransparency))
+if bearishFvg and show_IMB
+    box.new(left=bar_index - 1, top=low[2], right=bar_index + fvgboxLength, bottom=high[0], bgcolor=color.new(imbalancecolor, fvgTransparency), border_color=color.new(imbalancecolor, fvgTransparency))
+//////////////////// FVG //////////////////
+
 
 /////===============================================================================================
-//@version=5
-indicator("Triple EMA 50, 100, 150", overlay=true)
-
-// Definir los periodos de las EMAs
-ema_50 = ta.ema(close, 50)
-ema_100 = ta.ema(close, 100)
-ema_150 = ta.ema(close, 150)
-
-// Dibujar las EMAs en el gráfico con los colores deseados
-plot(ema_50, color=color.yellow, title="EMA 50", linewidth=2)
-plot(ema_100, color=color.blue, title="EMA 100", linewidth=2)
-plot(ema_150, color=color.red, title="EMA 150", linewidth=2)
-
-
-//======================================================================================
-//Dia operando en positivo con este codigo
 
 //@version=5
 indicator("Estrategia Mejorada con Señales Alcistas", overlay=true)
@@ -26,10 +141,9 @@ rsi = ta.rsi(close, rsiLength)
 smaLength = input.int(50, title="Longitud SMA")
 sma = ta.sma(close, smaLength)
 
-
 // Parámetros para Take Profit y Stop Loss
-takeProfitPercent = input.float(0.7, title="Take-Profit (%)", minval=0.0)  // Ajustado a 0.45%
-stopLossPercent = input.float(0.5, title="Stop-Loss (%)", minval=0.0)  // Puedes ajustar este valor si es necesario
+takeProfitPercent = input.float(0.71, title="Take-Profit (%)", minval=0.0)  // Ajustado a 0.7%
+stopLossPercent = input.float(2, title="Stop-Loss (%)", minval=0.0)  // Puedes ajustar este valor si es necesario
 
 // Variables para seguimiento
 var float entryPrice = na
@@ -37,8 +151,8 @@ var bool inPosition = false
 var float takeProfitPrice = na
 var float stopLossPrice = na
 
-// Condiciones para entrada
-longCondition = close > sma and rsi > 60 and ta.crossover(rsi, 60) // Precio por encima de SMA y RSI por encima de 55
+// Condiciones para entrada (ajustadas para más señales)
+longCondition = (close > sma and rsi > 50 and ta.crossover(rsi, 50)) or (close < sma and rsi < 40 and ta.crossunder(rsi, 40))
 
 if (longCondition and not inPosition)
     entryPrice := close
@@ -49,8 +163,8 @@ if (longCondition and not inPosition)
 
 // Lógica para salida
 if (inPosition)
-    // Si se alcanza el Take Profit
-    if (close >= takeProfitPrice)  
+    // Si se alcanza el Take Profit con la mecha
+    if (ta.highest(high, 1) >= takeProfitPrice)  
         label.new(bar_index, high, "Venta - Take Profit: " + str.tostring(takeProfitPrice, "#.##"), color=color.green, textcolor=color.white)
         inPosition := false
 
@@ -59,48 +173,7 @@ if (inPosition)
         label.new(bar_index, high, "Venta - Stop Loss: " + str.tostring(stopLossPrice, "#.##"), color=color.red, textcolor=color.white)
         inPosition := false
 
-// Visualización de niveles de TP y SL
-plot(inPosition ? takeProfitPrice : na, color=color.green, style=plot.style_stepline, title="Take Profit")
-plot(inPosition ? stopLossPrice : na, color=color.red, style=plot.style_stepline, title="Stop Loss")
-
-
-//======================================================================================================
-
-//@version=5
-strategy("TP 0.3% SL 1% - Solo Long con MA 200", overlay=true, default_qty_type=strategy.percent_of_equity, default_qty_value=10)
-
-// Parámetros de entrada
-tp_percent = input.float(1.0, title="Take Profit (%)", minval=0.1, step=0.1)  // 0.3% TP
-sl_percent = input.float(1.0, title="Stop Loss (%)", minval=0.1, step=0.1)    // 1% SL
-
-// Condiciones de entrada basadas en cruce de precio con la media móvil de 200 periodos
-ma_200 = ta.sma(close, 200)
-
-long_condition = close > ma_200  // Entrada en largo cuando el precio esté por encima de la MA de 200
-
-// Convertir los porcentajes en valores absolutos para el TP y SL
-long_tp = close * (1 + tp_percent / 100)
-long_sl = close * (1 - sl_percent / 100)
-
-// Ejecutar las operaciones solo en largo
-if long_condition
-    strategy.entry("Long", strategy.long)
-    strategy.exit("Exit Long", from_entry="Long", limit=long_tp, stop=long_sl)
-
-// Mostrar la media móvil de 200 periodos en el gráfico
-plot(ma_200, color=color.orange, linewidth=2)
-
-
-//================================================================================================================================================
-
-
-// Definir los periodos de las EMAs
-ema_50 = ta.ema(close, 50)
-ema_100 = ta.ema(close, 100)
-ema_150 = ta.ema(close, 150)
-
-// Dibujar las EMAs en el gráfico con los colores deseados
-plot(ema_50, color=color.gray, title="EMA 50", linewidth=2)
-plot(ema_100, color=color.gray, title="EMA 100", linewidth=1)
-plot(ema_150, color=color.red, title="EMA 150", linewidth=1)
+// Visualización de niveles de TP y SL en blanco
+plot(inPosition ? takeProfitPrice : na, color=color.white, style=plot.style_stepline, title="Take Profit")
+plot(inPosition ? stopLossPrice : na, color=color.white, style=plot.style_stepline, title="Stop Loss")
 
